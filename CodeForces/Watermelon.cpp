9585cacb58caad6925516a0836b86843b9700e06
Mihir Shrestha <121042506+MihiRrr24@users.noreply.github.com>
2025-05-18 23:19:48 +0530

    Updated

---
+++ CodeForces/Watermelon.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/4/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int w;
+    cin>>w;
+
+    cout<<(w%2==0 && w>=4 ? "Yes" : "No");
+}
